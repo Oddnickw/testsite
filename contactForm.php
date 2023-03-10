1d72ef9a3ab6dca146d6f98d310ba9333086b7b6
Glitch (wip-better-site) <none>
2023-03-10 21:40:47 +0000

    ☺️⛲️ Checkpoint ./contactForm.php:46808354/371 ./index.html:46808354/14

--- contactForm.php
+++ contactForm.php
@@ -1,1 +1,15 @@
-<?
+<?php
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "nick@nicholaswilde.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have gotten mail from".$name.". \n\n".$message;
+
+    mail($mailTo,$subject,$txt,$headers);
+}
+?php>
